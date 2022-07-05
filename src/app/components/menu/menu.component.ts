f1565d2774d1cd0661d900775924db3690f91ef3
joe barry-marron <joebm@joes-iMac.local>
2022-07-05 20:33:03 +1200

    creating skeleton components

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/model/MenuItem';
+import { MenuService } from 'src/app/services/menuService/menu.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  public menu : MenuItem[] = [];
+  constructor(
+    private menuService : MenuService
+  ) { }
+    
+  ngOnInit(): void {
+    this.menu = this.menuService.CreateMenu();
+  }
+}
