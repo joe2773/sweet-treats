e54e2373b9d61b0dc7ace08ea219212810f952f0
joe barry-marron <joebm@joes-iMac.ip>
2022-08-31 21:32:31 +1200

    adding data and changing service logic

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,12 +9,26 @@ import { MenuService } from 'src/app/services/menuService/menu.service';
 })
 export class MenuComponent implements OnInit {
   public menu : MenuItem[] = [];
+  public dessertCakes : MenuItem[] = [];
+  public favouriteCakes : MenuItem[] = [];
+  public signatureCakes : MenuItem[] = [];
+  public cupCakes : MenuItem[] = [];
+  public cakeSlabs : MenuItem[] = [];
+
   constructor(
     private menuService : MenuService
   ) { }
     
   ngOnInit(): void {
     this.menu = this.menuService.CreateMenu();
+
+    this.dessertCakes = this.menuService.GetDessertCakes();
+    this.favouriteCakes = this.menuService.GetFavouriteCakes();
+    this.signatureCakes = this.menuService.GetSignatureCakes();
+
+    //this.cupCakes = this.menuService.GetCupCakes();
+    //this.cakeSlabs = this.menuService.GetCakeSlabs();
+  
   }
 
   public menuItemClicked(index : number){
