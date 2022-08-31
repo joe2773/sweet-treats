c67fadc06dd7b8d9283e46916670352319ca8eae
joe2773 <jbarrymarron@gmail.com>
2022-08-31 22:35:21 +1200

    updating service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { MenuComponent } from './components/menu/menu.component';
 
 
 const routes: Routes = [
-  { path: '', component : MenuComponent },
+  { path: '', component : HomePageComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'contact', component : ContactUsComponent }
