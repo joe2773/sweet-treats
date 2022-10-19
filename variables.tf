c276abac9c76c8d193416360c461fa1872190e18
jbarry-marron <joe.barry-marron@trademe.co.nz>
2022-10-19 17:53:20 +1300

    creating terraform files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type    = string
+  default = "sweet-treats-355403"
+}
+
+variable "zone" {
+  type    = string
+  default = "australia-southeast1-a"
+}
+
+
+variable "region" {
+  type    = string
+  default = "australia-southeast1"
+}
